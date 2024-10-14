8771fe2fe573390cead6a5bd950d9a16d2bf70d4
Felix Obiang <obiang_felix@yahoo.com>
2024-10-14 14:11:01 +0200

    first commit

---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit,Input } from '@angular/core';
+import { FaceSnap } from '../models/face-snap';
+import { NgStyle, NgClass, LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe } from '@angular/common';
+@Component({
+  selector: 'app-face-snap',
+  standalone: true,
+  imports: [
+    NgStyle,
+    NgClass,
+    LowerCasePipe,
+    UpperCasePipe,
+    TitleCasePipe,
+    DatePipe
+    ],
+  templateUrl: './face-snap.component.html',
+  styleUrl: './face-snap.component.scss'
+})
+export class FaceSnapComponent implements OnInit{
+  @Input() faceSnap!: FaceSnap;
+
+  
+  snapButtonText!: string;
+  userHasSnapped!:boolean;
+
+  ngOnInit() {
+    
+    this.snapButtonText = 'Oh Snap!';
+    this.userHasSnapped= false;
+  }
+
+  onSnap() {
+    if(this.userHasSnapped)
+      {this.unSnap();}
+    else {this.snap();}
+  }
+  unSnap() {
+    this.faceSnap.removeSnap();
+    this.snapButtonText = 'Oh Snap!';
+    this.userHasSnapped = false;
+}
+
+snap() {
+    this.faceSnap.addSnap();
+    this.snapButtonText = 'Oops, unSnap!';
+    this.userHasSnapped = true;
+}
+
+  }
+
+
+
+
+
